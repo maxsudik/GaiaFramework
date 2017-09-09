1e5daf0907972a88131e45d4e3a56abc4c2a96c3
Ehsan Matean <ehsan.matean@fortify.pro>
2017-09-08 22:22:22 -0700

    added production tests

--- selenium/src/main/customerPanel/constants/UserInfo.java
+++ selenium/src/main/customerPanel/constants/UserInfo.java
@@ -11,4 +11,10 @@ public class UserInfo {
 	
 	public static final String USER_AUTO_SUPERVISOR = "zzzauto1";
 	public static final String PASSWORD_AUTO_SUPERVISOR = "zzzauto1";
+	
+	public static final String USER_PRODUCTION_MANAGER = "qamanager1";
+	public static final String PASSWORD_PRODUCTION_MANAGER = "QaTest$123!";
+	
+	public static final String USER_PRODUCTION_SUPERVISOR = "qasupervisor1";
+	public static final String PASSWORD_PRODUCTION_SUPERVISOR = "QaTest$123!";
 }
---
+++ selenium/src/test/java/web/productionTests/Verify_Login_Test.java
@@ -0,0 +1,54 @@
+package test.java.web.productionTests;
+
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import core.helpers.Helper;
+import core.logger.TestLog;
+import core.runner.ParallelRunner;
+import main.customerPanel.categories.login;
+import main.customerPanel.categories.user;
+import main.customerPanel.constants.UserInfo;
+import main.customerPanel.objects.UserObject;
+import main.customerPanel.pages.CustomerPanel;
+import test.java.TestBase;
+
+
+@RunWith(ParallelRunner.class)
+public class Verify_Login_Test extends TestBase {
+
+	@Before
+	public void beforeMethod() throws Exception {
+ 		setupWebDriver(CustomerPanel.GetDriver().withUrl(CustomerPanel.GAIA_SITE));
+	}
+
+	@Category({ login.class, user.class })
+	@Test
+	public void validate_user_login_as_manager() {
+		UserObject user = new UserObject().withEmail(UserInfo.USER_PRODUCTION_MANAGER).withPassword(UserInfo.PASSWORD_PRODUCTION_MANAGER);
+		
+		TestLog.When("I login with admin user");
+		app.customerPanel.login.login(user);
+		
+		TestLog.Then("I verify company list is displayed");
+		Helper.verifyElementIsDisplayed(app.customerPanel.company.byCompanyListSection());
+	}
+
+	
+	@Category({ login.class, user.class })
+	@Test
+	@Ignore //GA-947 but with supervisor panel not displaying
+	public void validate_user_login_as_supervisor() { 
+		UserObject user = new UserObject().withEmail(UserInfo.USER_PRODUCTION_SUPERVISOR).withPassword(UserInfo.PASSWORD_PRODUCTION_SUPERVISOR);
+		
+		TestLog.When("I login with supervisor user");
+		app.customerPanel.login.login(user);
+		
+		TestLog.Then("I verify people list is displayed");
+		Helper.verifyElementIsDisplayed(app.customerPanel.sites.byAddSiteButton());
+	}
+}
