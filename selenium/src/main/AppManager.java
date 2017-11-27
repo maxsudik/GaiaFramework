7db5706691321b76f5713ef7f311c4a31d203400
Ehsan Matean <ehsan.matean@fortify.pro>
2017-11-27 13:43:01 -0800

    removed rest reference

--- selenium/src/main/AppManager.java
+++ selenium/src/main/AppManager.java
@@ -2,7 +2,6 @@ package main;
 
 import main.customerPanel.Panels.CustomerPanelManager;
 import main.main_ios.Panels.GaiaManager_ios;
-import main.main_rest.GaiaRest;
 
 /**
  * Manages all existing apps Abstract driver refers to this manager to call
@@ -13,5 +12,4 @@ public class AppManager{
 
 	public CustomerPanelManager customerPanel = new CustomerPanelManager();
 	public GaiaManager_ios gaia = new GaiaManager_ios();
-	public GaiaRest rest = new GaiaRest();
 }
