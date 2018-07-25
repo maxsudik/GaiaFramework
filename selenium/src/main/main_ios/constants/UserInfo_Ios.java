a57181f402cf118dcf8780a1103e735fa5fd39e9
Ehsan Matean <ehsan.matean@fortify.pro>
2018-07-24 21:03:06 -0700

    updated user info

--- selenium/src/main/main_ios/constants/UserInfo_Ios.java
+++ selenium/src/main/main_ios/constants/UserInfo_Ios.java
@@ -6,5 +6,5 @@ public class UserInfo_Ios {
 	public static final String MANAGER_PASSWORD = "zzzautomanager1";
 	
 	public static final String SUPERVISOR_USERNAME = "zzzauto1";
-	public static final String SUPERVISOR_PASSWORD = "zzzauto1";
+	public static final String SUPERVISOR_PASSWORD = "zzzautosupervisor1";
 }
