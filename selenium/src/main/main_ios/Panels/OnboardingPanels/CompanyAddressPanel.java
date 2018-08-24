cb6ffbe64bd2007c882fbda4f544f3b1eabd90b3
Maksym Sudik <maksym.sudik@fortify.pro>
2018-08-24 12:57:17 -0700

    Trying create @FreeBuilder

--- selenium/src/main/main_ios/Panels/OnboardingPanels/CompanyAddressPanel.java
+++ selenium/src/main/main_ios/Panels/OnboardingPanels/CompanyAddressPanel.java
@@ -1,5 +1,31 @@
 package main.main_ios.Panels.OnboardingPanels;
 
+import org.inferred.freebuilder.FreeBuilder;
+
+import core.helpers.Element;
+import core.webElement.EnhancedBy;
+import main.main_ios.GaiaIos;
+
+@FreeBuilder
 public class CompanyAddressPanel {
+	
+GaiaIos manager;
+	
+	public CompanyAddressPanel( GaiaIos manager) {
+		this.manager = manager;
+	}
+	
+	public static  EnhancedBy ADDRESS_LINE_1_FIELD = Element.byClass("XCUIElementTypeTextField", "Address Field");
+	public static  EnhancedBy ADDRESS_LINE_2_FIELD = Element.byClass("XCUIElementTypeTextField", "Product Name Field");
+	public static  EnhancedBy CITY_FIELD = Element.byClass("XCUIElementTypeTextField", "City");
+	public static  EnhancedBy COUNTRY_DDL = Element.byClass("XCUIElementTypeButton", "Country");
+	public static EnhancedBy withCanadaDDLMI(String country) {
+		return Element.byAccessibility(country, country);
+	}
+	public static  EnhancedBy STATE_DDL = Element.byClass("XCUIElementTypeButton", "State");
+	public static  EnhancedBy POSTAL_CODE_FIELD = Element.byClass("XCUIElementTypeTextField", "Postal Code");
+	public static  EnhancedBy CONFIRM_BUTTON = Element.byClass("XCUIElementTypeButton", "Confirm");
+	
+	
 
 }
