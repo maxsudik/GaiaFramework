9c10a292cfc2416028cfa98f1909963a8bb89256
Ehsan Matean <ehsan.matean@fortify.pro>
2018-09-27 23:07:56 -0700

    updated test suit

--- selenium/src/test/java/TestBase.java
+++ selenium/src/test/java/TestBase.java
@@ -3,6 +3,7 @@ package test.java;
 import core.driver.AbstractDriverTestNG;
 import main.AppManager;
 
+
 public class TestBase extends AbstractDriverTestNG {
 	protected AppManager app = new AppManager();
 }
